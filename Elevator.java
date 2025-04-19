b77de226eb07e1148aefde6a1b3abf285cd147d6
JoyBoyCr7 <Rnyongkah@gmail.com>
2025-04-18 19:06:26 -0500

    final

--- Elevator.java
+++ Elevator.java
@@ -1,30 +1,26 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.util.EventListener;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
-import java.io.File;
-
+/**Elevator Project
+ *@author Ronard Nyongkah
+ *@version Spring 2025
+ *CSci1130
+*/
 
 public class Elevator extends JFrame implements ActionListener, ItemListener{
     private final int DISPLAY_WIDTH = 800;
     private final int DISPLAY_HEIGHT = 900;
 
-    int yOffset;
-
-    private JPanel guiPanel, choicePanel, rightPanel, leftPanel;
+    private JPanel choicePanel;
     JButton submitButton;
     JComboBox choiceBox;
     
-
-    int num, choice, currentFloor = 1, nextFloor = 0, floorDifference;
-    int locX, locY, yChange = 0, elevatorY;
-
-   
+    int num, choice = 1, currentFloor = 1, nextFloor = 0, floorDifference;
+    int locX, yChange = 0, elevatorY;
 
     final int LIMIT = 10;
     final int NUM_WINDOWS = 10;
@@ -33,17 +29,10 @@ public class Elevator extends JFrame implements ActionListener, ItemListener{
     final int WINDOW_GAP = 15;
     final int X_OFFSET = 0;
     final int COUNT_START = 1;
-    boolean changeColor;
-
-    JComboBox choiBox;
 
     int floorHeight = WINDOW_HEIGHT + WINDOW_GAP;
 
     private DisplayPanel display;
-
-    
-
-
     public static void main(String[] args){
         Elevator frame = new Elevator();
         frame.setLayout(new BorderLayout());
@@ -67,7 +56,7 @@ public class Elevator extends JFrame implements ActionListener, ItemListener{
         window.add(display, BorderLayout.CENTER);
     }
 
-
+// creating my elevator change panel at the bottom of the window
     public void setUpCombo(){
         choicePanel = new JPanel(new FlowLayout());
         choiceBox = new JComboBox();
@@ -86,40 +75,29 @@ public class Elevator extends JFrame implements ActionListener, ItemListener{
         
     }
 
-
+    // Variable creation
     public void initializeVariables(){
         num = COUNT_START;
         locX = X_OFFSET;
-        // locY = DISPLAY_HEIGHT / 2;
-
     }
-
-    // public void UpdateElevatorY (){
-    //     floorDifference = currentFloor - nextFloor; 
-    //     elevatorY += floorDifference * floorHeight;
-    //     currentFloor = nextFloor;
-    // }
+    // changes to the next floor
     public void UpdateElevatorY() {
         floorDifference = nextFloor - currentFloor; 
-        elevatorY = (LIMIT - nextFloor) * floorHeight; // Adjust to move bottom to top
+        elevatorY = (LIMIT - nextFloor) * floorHeight; 
         currentFloor = nextFloor;
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        // floorDifference = currentFloor - nextFloor;
-        // elevatorY = floorDifference * WINDOW_HEIGHT;
-        // currentFloor = nextFloor;
         nextFloor = choice;
         UpdateElevatorY();
         
         display.repaint();
     }
-
+    // creating elevator animation 
     public void inMotion(Graphics2D g2d) {
         g2d.setColor(Color.YELLOW);
-        g2d.fillRect(locX,elevatorY,WINDOW_WIDTH,WINDOW_HEIGHT
-        );
+        g2d.fillRect(locX,elevatorY,WINDOW_WIDTH,WINDOW_HEIGHT);
     }
 
     @Override
@@ -140,67 +118,30 @@ public class Elevator extends JFrame implements ActionListener, ItemListener{
             g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                     RenderingHints.VALUE_ANTIALIAS_ON);
 
-                
-            // whileTest(g2d);
-            //forTest(g2d);
-            
             drawFloor(g2d);
             inMotion(g2d);
 
         }
 
-        
-
         public void drawFloor(Graphics2D g2d) {
-
+        // creating building 
         int buildingWidth = NUM_WINDOWS * WINDOW_WIDTH + (NUM_WINDOWS - 1) * WINDOW_GAP;
         int buildingHeight = NUM_WINDOWS * WINDOW_HEIGHT + (NUM_WINDOWS - 1) * WINDOW_GAP;
 
         // Draw background (the building)
         g2d.setColor(new Color(150, 50, 50)); 
-        g2d.fillRect(locX - 5, locY - 5, buildingWidth + 10, buildingHeight + 10);
+        g2d.fillRect(locX - 5, 5, buildingWidth + 10, buildingHeight + 10);
 
-
+            // creating my floors
             for (int j = 0; j < NUM_WINDOWS; j++) {
+                //creating my windows
                 for (int i = 0; i < NUM_WINDOWS; i++) {
                     g2d.setColor(Color.BLACK);
-                    g2d.fillRect(locX + (i * (WINDOW_WIDTH + WINDOW_GAP)), locY + (j * (WINDOW_HEIGHT + WINDOW_GAP)), WINDOW_WIDTH, WINDOW_HEIGHT);
+                    g2d.fillRect(locX + (i * (WINDOW_WIDTH + WINDOW_GAP)), (j * (WINDOW_HEIGHT + WINDOW_GAP)), WINDOW_WIDTH, WINDOW_HEIGHT);
                 }
                 
             }
 
-            // for (int i = 0; i < NUM_WINDOWS; i++) {
-            //     if (choice == i + 1){
-            //         g2d.setColor(Color.YELLOW);
-            //     }
-            //     g2d.fillRect(locX + (i * (WINDOW_WIDTH + WINDOW_GAP)), locY + yOffset , WINDOW_WIDTH, WINDOW_HEIGHT);
-            // }
-            
-        }
-
-        // public void drawFloors(Graphics2D g2d){
-        //     for (int j = 0; j <= NUM_WINDOWS; j++){
-        //         yOffset = j * (WINDOW_HEIGHT + WINDOW_GAP);
-        //         drawFloor(g2d,yOffset);
-        //         // locY +=5;
-        //     }
-        // }
-
-        public void whileTest(Graphics2D g2d){
-            //modify this so that it will print out 1-100
-            //each line will have only 30 numbers
-            //hint, this will use if statements and a change in locY
-            // while(num<=LIMIT){
-            //     g2d.drawString(num+", ", locX+num* LETTER_SPACE, locY);
-            //     num++;
-            // }
-        }
-
-        public void forTest(Graphics2D g2d){
-            //modify this so that each number alternates between green and red
-            // for(int i=1; i<=LIMIT; i++){
-            //     g2d.drawString(i+", ", locX+i* LETTER_SPACE, locY);
-            // }
         }
     }
     
